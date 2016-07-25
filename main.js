a829d93d40f932a1d2129643b727ca751deb965b
tyson-tlo <tyson_tlo@live.ca>
2016-07-25 13:45:20 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+/*Main.js*/
+/*Here is where all of the functionality for the web app will be created. Please note: the goal of this application is to create
+a mobile first application for smart phones primarily as a mobile supplement to the full course load found at both fluentspeakenglish.com
+& fluentlyspeak.com */
